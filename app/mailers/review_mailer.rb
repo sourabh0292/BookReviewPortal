5a9254d410fc2534bd36f4258d757025775615f5
Sourabh Tolani <sourabhtolani02@gmail.com>
2017-04-27 12:11:20 +0530

    Added users for authors

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -5,6 +5,7 @@ class ReviewMailer < ApplicationMailer
 	@review = Review.find(review_id)
 	@comment = @review.comment
 	@rating = @review.rating
+	@book = @review.book.title
 	@user = @review.u_ser
 	mail to: @user.email, subject: "Feedback Submitted", from: 'sourabhtolani02@gmail.com'
 	end
